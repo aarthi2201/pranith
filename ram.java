a92227d6b124415f709ca11a2876efdf4f5e6c8e
nandhini <nandhinimk2019@gmail.com>
2022-10-11 15:51:00 +0530

    first commit

---
+++ ram.java
@@ -0,0 +1,55 @@
+import java.io.*;
+
+abstract class ram
+{
+double l=10,b=5;
+abstract void printarea();
+}
+
+class rectangle extends shape
+
+{
+double area=l*b;
+void printarea()
+{
+System.out.println("rect"+area);
+}
+}
+
+class triangle extends shape
+
+{
+double area1=0.5*l*b;
+void printarea()
+{
+System.out.println("tri"+area1);
+}
+
+}
+
+class circle extends shape
+
+{
+
+double r=20;
+double area2=0.314*r*r;
+void printarea()
+{
+System.out.println("circle"+area2);
+}
+}
+ 
+
+public class a
+{
+public static void main(String args[])
+{
+rectangle r=new rectangle();
+r.printarea();
+triangle t=new triangle();
+t.printarea();
+circle c=new circle();
+c.printarea();
+
+}}
+
